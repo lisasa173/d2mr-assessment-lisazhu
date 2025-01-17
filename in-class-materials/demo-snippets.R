2e3c5d10debed1d626ce4f9cd093ed9f48081944
lisasa173 <lisazhu@uchicago.edu>
2025-01-17 09:47:18 -0600

    new change

--- in-class-materials/demo-snippets.R
+++ in-class-materials/demo-snippets.R
@@ -24,19 +24,27 @@ add.two2(3)
 fnc_paste_return <- function(argument1, argument2) {
     return(paste(argument1, argument2))
 }
+fnc_paste_return (1,2)
 
 fnc_paste <- function(argument1, argument2) {
     paste(argument1, argument2)
 }
+fnc_paste(1,2)
 
 fnc_paste_store <- function(argument1, argument2) {
     myvalue <- paste(argument1, argument2)
 }
+fnc_paste_store(1,2)
 
 fnc_paste_return("hello", "world")
 fnc_paste("hello", "world")
 fnc_paste_store("hello", "world")
-
+#Why is the third one not displaying any visible output?
+fnc_paste_store_edited <- function(argument1, argument2) {
+  myvalue <- paste(argument1, argument2)
+  print (myvalue)
+}
+fnc_paste_store_edited (1,2)
 
 #### See groceries.qmd for all the grocer's partner conditional/functions shenanigans! ####
 
@@ -48,12 +56,13 @@ for (i in c(1,2,3,4)) {print(2^i)}
 timestwo <- function(number) {x <- number*2
     print(x) 
 }
-
+timestwo(1)
 
 i <- 1
 while(i <= 4) {
     print(2^i)
     i <- i + 1}
+#starting from 1, looping with incremental value of 1, until 4, provided the function 2^1.
 
 # Hello world demo
 
